8a9e08aa9d69842a6725cb0192411e7db7f4f86b
Teemu Tanninen <teemuxtanninen@gmail.com>
2024-09-23 14:25:21 +0300

    refactor: remove try-catch block from fetchJsonData function.

--- index.js
+++ index.js
@@ -15,21 +15,15 @@ window.onload = async () => {
 };
 
 async function fetchJsonData(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    const contentType = response.headers.get("content-type");
-    if (!contentType || !contentType.includes("application/json")) {
-      throw new TypeError("Fetched dataset isn't JSON!");
-    }
-    return await response.json();
-  } catch (err) {
-    console.error(
-      `Error occurred in fetch function | ${err.name}: ${err.message}`
-    );
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
   }
+  const contentType = response.headers.get("content-type");
+  if (!contentType || !contentType.includes("application/json")) {
+    throw new TypeError("Fetched dataset isn't JSON!");
+  }
+    return await response.json();
 }
 
 function fillTableWithData(munPopData, employData) {
