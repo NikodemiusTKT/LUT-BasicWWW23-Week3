3949d7d8037a71f487dd9c7326218e3011076243
Teemu Tanninen <teemuxtanninen@gmail.com>
2024-09-23 13:58:45 +0300

    refactor: Change onLoad function to use Promise.all to fetch both datasets in parallel.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 window.onload = async () => {
   const municipalityDataUrl =
-    'https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff';
+    "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
   const employmentDataURL =
-    'https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065';
-  const munPopData = await fetchJsonData(municipalityDataUrl);
-  const employData = await fetchJsonData(employmentDataURL);
+    "https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065";
+  try {
+    const [munPopData, employData] = await Promise.all([
+      fetchJsonData(municipalityDataUrl),
+      fetchJsonData(employmentDataURL),
+    ]);
     fillTableWithData(munPopData, employData);
+  } catch (error) {
+      console.error(`Data loading failed: ${error.message}`);
+  }
 };
 
 async function fetchJsonData(url) {
@@ -14,8 +20,8 @@ async function fetchJsonData(url) {
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
-    const contentType = response.headers.get('content-type');
-    if (!contentType || !contentType.includes('application/json')) {
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
       throw new TypeError("Fetched dataset isn't JSON!");
     }
     return await response.json();
@@ -37,17 +43,17 @@ function fillTableWithData(munPopData, employData) {
     const employRate = curEmp / curPop;
     const roundedEmployRate = (employRate * 100).toFixed(2);
     let tRow = createTableRowElem(mun, curPop, curEmp, `${roundedEmployRate}%`);
-    if (employRate > 0.45) tRow.style.backgroundColor = '#abffbd';
-    else if (employRate < 0.25) tRow.style.backgroundColor = '#ff9e9e';
+    if (employRate > 0.45) tRow.style.backgroundColor = "#abffbd";
+    else if (employRate < 0.25) tRow.style.backgroundColor = "#ff9e9e";
     tRowArray.push(tRow);
   });
-  document.querySelector('tbody').append(...tRowArray);
+  document.querySelector("tbody").append(...tRowArray);
 }
 
 function createTableRowElem(...textArgs) {
-  let tRow = document.createElement('tr');
+  let tRow = document.createElement("tr");
   for (let arg of textArgs) {
-    let tCol = document.createElement('td');
+    let tCol = document.createElement("td");
     tCol.innerText = arg;
     tRow.append(tCol);
   }
